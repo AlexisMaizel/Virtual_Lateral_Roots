b597d8140ad7d3949cfe71c47092dec81def890b
Jens Fangerau (Home) <jens.fangerau@iwr.uni-heidelberg.de>
2014-07-17 12:17:56 +0200

    Added method to check if point is in interior of triangle

--- Modelling/VirtualLateralRoot/SurfacePoints.cpp
+++ Modelling/VirtualLateralRoot/SurfacePoints.cpp
@@ -5,3 +5,23 @@
   @brief  Class for handling triangulation of real data points
   @author Jens Fangerau <jens.fangerau@iwr.uni-heidelberg.de>
 */
+
+//----------------------------------------------------------------
+
+SurfacePoints::SurfacePoints()
+{
+}
+
+//----------------------------------------------------------------
+
+bool SurfacePoints::pointIsInTriangle( const Point2d &p, const Point2d &p0,
+                                       const Point2d &p1, const Point2d &p2 )
+{
+  double area = 0.5*(-p1.j()*p2.i() + p0.j()*(-p1.i() + p2.i()) + p0.i()*(p1.j() - p2.j()) + p1.i()*p2.j());
+  double s = 1./(2.*area)*(p0.j()*p2.i() - p0.i()*p2.j() + (p2.j() - p0.j())*p.i() + (p0.i() - p2.i())*p.j());
+  double t = 1./(2.*area)*(p0.i()*p1.j() - p0.j()*p1.i() + (p0.j() - p1.j())*p.i() + (p1.i() - p0.i())*p.j());
+  
+  return (s > 0. && t > 0. && 1.-s-t > 0);
+}
+
+//----------------------------------------------------------------
--- Modelling/VirtualLateralRoot/SurfacePoints.h
+++ Modelling/VirtualLateralRoot/SurfacePoints.h
@@ -7,4 +7,18 @@
   @author Jens Fangerau <jens.fangerau@iwr.uni-heidelberg.de>
 */
 
+#include <geometry/geometry.h>
+
+class SurfacePoints
+{
+public:
+  
+  SurfacePoints();
+  bool pointIsInTriangle( const Point2d &p, const Point2d &p0,
+                          const Point2d &p1, const Point2d &p2 );
+  
+private:
+  
+};
+
 #endif // SurfacePoints_HH
