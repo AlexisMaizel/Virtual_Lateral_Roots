69bffeb3bb8afc163feb91eb0e08a33dc395c4d7
Jens Fangerau (Home) <jens.fangerau@iwr.uni-heidelberg.de>
2014-07-17 11:58:16 +0200

    New class for handling triangulation surfaces made by real data points

---
+++ Modelling/VirtualLateralRoot/SurfacePoints.cpp
@@ -0,0 +1,7 @@
+#include "SurfacePoints.h"
+
+/**
+  @file   SurfacePoints.cpp
+  @brief  Class for handling triangulation of real data points
+  @author Jens Fangerau <jens.fangerau@iwr.uni-heidelberg.de>
+*/
---
+++ Modelling/VirtualLateralRoot/SurfacePoints.h
@@ -0,0 +1,10 @@
+#ifndef SurfacePoints_HH
+#define SurfacePoints_HH
+
+/**
+  @file   SurfacePoints.h
+  @brief  Class for handling triangulation of real data points
+  @author Jens Fangerau <jens.fangerau@iwr.uni-heidelberg.de>
+*/
+
+#endif // SurfacePoints_HH
